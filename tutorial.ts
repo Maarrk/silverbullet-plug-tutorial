25009ff649881a85887cebcdd597cc83a72260b2
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-01-14 20:02:43 +0100

    Basic command with online data

--- tutorial.ts
+++ tutorial.ts
@@ -3,3 +3,14 @@ import { editor } from "$sb/silverbullet-syscall/mod.ts";
 export async function helloWorld() {
   await editor.flashNotification("Hello world!");
 }
+
+export async function randomUser() {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  if (result.status < 200 || result.status >= 300) {
+    // Handle HTTP error here
+    throw new Error(await result.text());
+  }
+  const data = await result.json();
+
+  await editor.flashNotification(data["name"]);
+}
