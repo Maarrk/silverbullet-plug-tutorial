6f301af6e951bbbecef604592145929e32bbd85f
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-01-15 16:06:20 +0100

    Basic config

--- tutorial.ts
+++ tutorial.ts
@@ -1,10 +1,14 @@
 import { editor } from "$sb/silverbullet-syscall/mod.ts";
+import { readSettings } from "$sb/lib/settings_page.ts";
 
 export async function helloWorld() {
   await editor.flashNotification("Hello world!");
 }
 
 export async function randomUser() {
+  const config = await readSettings({ userCount: 1 });
+  console.log(config);
+
   const result = await fetch("https://jsonplaceholder.typicode.com/users/1");
   if (result.status < 200 || result.status >= 300) {
     // Handle HTTP error here
